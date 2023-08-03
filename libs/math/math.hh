f154a6d85a708a87cc72a3c7370425899e04d107
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-08-03 22:29:49 +0700

    feat: add Algorithm of Combinatorial
    ## Description
    this update to add some of algorithm combinatorial
    combinatorial is an area of mathematics primarily concerned
    with counting,both as a means and end in obtaining result,and certain
    properties of finite structures(see combinatorial logic,Taylor Series).
    
    - # Algorithm
        - Binominal Series
            >  the binomial series is a generalization of the polynomial that comes from a binomial formula expression like (1 + x)^n
               for a nonnegative integer n.Specifically,the binomial series is the ``Taylor Series``.
    
        - log base
            > Log base is same like natural logarithm (use Method Mercator Series)
    
        - Log 10
            > same like Natural logarithm
    
        - Fibonacci
          > Fibonacci is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. The sequence is named after Leonardo of Pisa, known as Fibonacci, an Italian
          mathematician who introduced it to the Western world in his book "Liber Abaci
    
        - Bell Numbers
          > In Combinatorial Mathematics,The Bell Numbers count the possible  partitions of a set. These numbers have been studied by mathematicians since the 19th century,
          and their roots go back to medieval Japan. In an example of Stigler's law of eponymy, they are named after Eric Temple Bell, who wrote about them in the 1930s.

--- libs/math/math.hh
+++ libs/math/math.hh
@@ -32,6 +32,7 @@ int factorial_number(int number) {
     return factorial;
 }
 
+
 template <typename T>
 T power(T number, double rank) {
     if (rank < 0) {
@@ -47,11 +48,13 @@ T power(T number, double rank) {
     }
 }
 
+
 int combi(int n, int k) {
     int numerator = factorial_number(n);
     int denominator = factorial_number(k) * factorial_number(n - k);
     return numerator / denominator;
 }
+
 
 int permutation(int n, int k = 0) {
     if (k == 0) {
@@ -62,7 +65,11 @@ int permutation(int n, int k = 0) {
     return numerator / denominator;
 }
 
+
 double e() {
+    // the name of algorithm is talyor series
+    // this variabel iterable for result be good and approach
+    // with a real value
     int n = 10;
     double result = 1.0;
     for (int i = 1; i <= n; ++i) {
@@ -71,10 +78,16 @@ double e() {
     return result;
 }
 
+
 double exponent(double x) {
-     // Use std::exp() for exponentiation
+    /*
+     * @brief exponent is math formula from
+     * equation power of euler number and param
+     * of x
+    */
     return power(e(),x);
 }
+
 
 template <typename T>
 T abs(T value) {
@@ -84,6 +97,17 @@ T abs(T value) {
         return value;
     }
 }
+
+
+long int  binomial(int x,int n){
+    double result =0;
+    for (int k=0;k<=n;++k){
+        result += combi(n,k) * power(x,k); 
+    }
+    return result;
+
+}
+
 
 template <typename T>
 double natural_log(T x) {
@@ -98,6 +122,12 @@ double natural_log(T x) {
     } while (abs(yn - yn1) > epsilon);
     return yn1;
 }
+
+
+// @brief square function is to calculate 
+// a square of value.
+// @param x this a input value we wont calculate
+// return a result decimal number
 
 template <typename T>
 float square_value(T x) {
@@ -114,6 +144,48 @@ float square_value(T x) {
         }
     }
     return r;
+}
+
+
+template <typename T>float log_base(T X,int base){
+    if (base <0){
+        std::cerr<<"Base value must greater than or equal to\n";
+    }
+    return natural_log(X) / natural_log(base);
+}
+
+
+template <typename T > double log10(T x){
+    if (x<0){
+        std::cerr<<"Value lower than 0 can't be calculate"<<std::endl;
+    }
+    return natural_log(x)/natural_log(10);
+}
+
+
+long int fibonacci(int n){
+    int a=0,b=1;
+    for (int i=2;i<=n;++i){
+        int temp = a;
+        a = b;
+        b = temp+a;
+    }
+    return b;
+}
+
+
+long long int bell_num(int n) {
+    int bell[n + 1][n + 1];
+    bell[0][0] = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        bell[i][0] = bell[i - 1][i - 1];
+        for (int j = 1; j <= i; ++j) {
+            bell[i][j] = bell[i - 1][j - 1] + bell[i][j - 1];
+        }
+    }
+
+    return bell[n][0];
 }
 
 
